e813d29a1f38cf2fc1034972495cab7f3df851f8
Milan TURCANO <turcano.milan@sfr.fr>
2024-10-03 20:54:37 +0200

    modif du paquet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "",
@@ -15,9 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="h-full" lang="en">
-      <body className={`h-full ${inter.className}`}>
-        <main className="h-full">{children}</main>
+    <html lang="fr">
+      <body className={`antialiased`}>
+        <main>{children}</main>
       </body>
     </html>
   );
